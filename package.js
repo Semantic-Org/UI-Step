e0c52cf4d49c822e447e3e4fa07b244e3ef3fbdf
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:26:29 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-step',
+  summary : 'Semantic UI - Step (official): Single component release of step',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Step.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
