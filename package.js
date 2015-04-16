d28ce01b37af159efce8c4c51124bf140f520077
Pusher Robot <jack@semantic-ui.com>
2015-04-16 12:38:55 -0400

    Updated component to version 1.12.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-step',
   summary : 'Semantic UI - Step: Single component release',
-  version : '1.11.7',
+  version : '1.12.0',
   git     : 'git://github.com/Semantic-Org/UI-Step.git',
 });
 
